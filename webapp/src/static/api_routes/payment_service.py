35abbb9259d69a19280e4512546ab0b67add3c49
Laserkillers <spyon.alex@yandex.ru>
2023-09-30 22:03:15 +0300

    create api for payment_service

---
+++ webapp/src/static/api_routes/payment_service.py
@@ -0,0 +1,16 @@
+import json
+from playhouse.shortcuts import model_to_dict
+from os import getenv as env
+
+from flask import make_response, request
+
+from src.static.entities.person import Person
+
+from src.static import routes, payment_service_path
+
+flask_blueprint = routes
+
+
+@flask_blueprint.route(payment_service_path + '/payments', methods=['GET'])
+def get_all_payments():
+    return make_response({}, 200)
