6c5caccb3d94330dfd1ebdde76431c1013bfaa52
Laserkillers <spyon.alex@yandex.ru>
2023-09-30 20:37:56 +0300

    remake entity

--- webapp/src/static/entities/person.py
+++ webapp/src/static/entities/person.py
@@ -1,10 +1,4 @@
-from src.config.program_config import DataBaseSettings
-from peewee import *
-
-
-class BaseModel(Model):
-    class Meta:
-        database = PostgresqlDatabase(**DataBaseSettings().cursor_connection_row())
+from . import *
 
 
 class Person(BaseModel):
