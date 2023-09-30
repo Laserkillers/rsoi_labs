980722843cc1f748312b058740057fa52a71a4dd
Laserkillers <spyon.alex@yandex.ru>
2023-09-30 20:36:46 +0300

    delete old sql import

--- migrations/create_person_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE person(
-    id serial not null,
-    name text not null,
-    age integer not null,
-    address text not null,
-    work text not null
-);
--- migrations/fill_data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO person (name, age, address, work) VALUES ('Alex', 22, 'bmstu', 'rtl');
