9d18b3bb56f5d723f6e4fcb34099f36c6832a303
Laserkillers <spyon.alex@yandex.ru>
2023-09-30 21:47:13 +0300

    add services paths and functions

--- webapp/src/static/api_routes/__init__.py
+++ webapp/src/static/api_routes/__init__.py
@@ -1,4 +1,10 @@
 base_path = '/api/v1/'
 
+reserve_service_path = '/api/reserve'
+payment_service_path = '/api/payment'
+loyalty_service_path = '/api/loyalty'
+
 from src.static.api_routes.check_work import *
-from src.static.api_routes.work_with_person import *
+# from src.static.api_routes.work_with_person import *
+from src.static.api_routes.gateway import *
+from src.static.api_routes.reserve_service import *
