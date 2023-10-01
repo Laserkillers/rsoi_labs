e9f663e24eb2b9117ea1c7376bb7a3cab389b910
Laserkillers <spyon.alex@yandex.ru>
2023-10-01 15:56:54 +0300

    done get info by user id

--- webapp/src/static/api_routes/loyalty_service.py
+++ webapp/src/static/api_routes/loyalty_service.py
@@ -4,13 +4,30 @@ from os import getenv as env
 
 from flask import make_response, request
 
+from src.static.entities.loyalty import Loyalty
 from src.static.entities.person import Person
 
-from src.static import routes, loyalty_service_path
+from . import routes, loyalty_service_path
 
 flask_blueprint = routes
 
 
 @flask_blueprint.route(loyalty_service_path + '/loyalty', methods=['GET'])
-def get_all_payments():
+def get_all_loyalty():
     return make_response({}, 200)
+
+
+@flask_blueprint.route(loyalty_service_path + '/user_info/<user_uuid>')
+def get_user_loyalty_info(user_uuid=None):
+    if user_uuid is None:
+        return make_response(
+            {'message': 'Empty response'},
+            400
+        )
+
+    res_loyalty = Loyalty.get(Loyalty.username == user_uuid)
+
+    return make_response(
+        model_to_dict(res_loyalty),
+        200
+    )
