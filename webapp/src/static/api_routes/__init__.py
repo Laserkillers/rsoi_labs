5cc40dc599ebdd5b8d6fde5920d6650a8cf8ac09
Laserkillers <spyon.alex@yandex.ru>
2023-10-01 15:56:12 +0300

    add all requests

--- webapp/src/static/api_routes/__init__.py
+++ webapp/src/static/api_routes/__init__.py
@@ -1,4 +1,4 @@
-base_path = '/api/v1/'
+base_path = '/api/v1'
 
 reserve_service_path = '/api/reserve'
 payment_service_path = '/api/payment'
@@ -8,3 +8,5 @@ from src.static.api_routes.check_work import *
 # from src.static.api_routes.work_with_person import *
 from src.static.api_routes.gateway import *
 from src.static.api_routes.reserve_service import *
+from src.static.api_routes.loyalty_service import *
+from src.static.api_routes.payment_service import *
